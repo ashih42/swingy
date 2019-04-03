ce4eca8e34eacc6ce7d7afe8b57ecd23e66c320c
Andy <AndyShih51@gmail.com>
2019-04-03 12:37:10 -0700

    Move env variable into run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Export env variable containing IP address of mysql server
+eval "$(docker-machine env Char)";
+export DOCKER_MACHINE_IP=$(docker-machine ip Char);
+
+# Run the game in a jar file
 java -jar target/swingy-1.0-SNAPSHOT.jar $@
--- setup/env_init.sh
+++
@@ -1,4 +0,0 @@
-# source env_init.sh
-
-eval "$(docker-machine env Char)";
-export DOCKER_MACHINE_IP=$(docker-machine ip Char);
